b0b1f21fe173c7be219bcc9f7c87928f09d19801
lundbmp <m.lundberg008@gmail.com>
2022-06-25 12:47:15 -0500

    making new branch

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +9,10 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+
+};
 
 // Function call to initialize app
 init();
